ac7f0fb886ca3ec5f0b6b4dd8195a96a0545c877
Soul Sender <>
2021-05-25 23:38:43 +0000

    Added server players currently active

--- main.py
+++ main.py
@@ -19,14 +19,13 @@ def get_insult():
 def get_serverinfo():
   response = requests.get("https://api.mcsrvstat.us/bedrock/2/142.44.145.32:25626")
   json_data = response.json()
-  say_serverinfo = json_data["online"]
+  say_serverinfo = json_data["players"]['online']
   return say_serverinfo
 
 def get_serverplayers():
   response = requests.get("https://api.mcsrvstat.us/bedrock/2/142.44.145.32:25626")
   json_data = response.json()
-  say_serverplayers = json_data['players']['max']
-  print(json_data['players']['max'])
+  say_serverplayers = json_data['players']['online']
   return say_serverplayers
 
 
@@ -42,43 +41,31 @@ async def on_ready():
     if message.author == client.user:
       return
 
-    # create apis
 
     if message.content.startswith('$insult'):
       await message.channel.send(get_insult())
 
 
     if message.content.startswith('$server'):
+      players_online = get_serverplayers()
       is_online = get_serverinfo()
       if is_online:
         onlineview = ':white_check_mark: - Server is up and running'   
       if not is_online:
         onlineview = ':x: - Server is down '
 
-      embedVar = discord.Embed(title="__Server Info__", description="Collection of infomation regarding server IP, status, and changes.", color=0x00ff00)
+      embedVar = discord.Embed(title="__Server Info__", description="Collection of infomation regarding server IP, status, and changes. \n Please keep in mind data *ONLY* updates every 5 minutes or so.", color=0x00ff00)
       embedVar.add_field(name="Connection Info", value="IP - 142.44.145.32 \n PORT - 25626",inline=False)
       embedVar.add_field(name="Current Status", value=onlineview, inline=False)
-      await message.channel.send(embed=embedVar)
+      embedVar.add_field(name="Players Online", value=players_online + '/20', inline=False)
+      await message.channel.send(embed=embedVar)   
       
-
-      
-      
-        
-        
-
-      #await message.channel.send(get_serverplayers())
-      
-      
-  
-
-
-
 
     # create basic replies
 
     if message.content.startswith('$help'):
       embedVar = discord.Embed(title="__Command Menu__", color=0x2b2a2a)
-      embedVar.add_field(name="Useful", value="$help - send help menu \n $server - get infomation about our minecraft server",inline=False)
+      embedVar.add_field(name="Useful", value="$help - send help menu \n $server - get infomation about our minecraft server \n $market - open the market (WIP)",inline=False)
       embedVar.add_field(name="Miscellanous", value='$insult - send randomly generated insult \n $hello - say hello! \n $sheesh - sheesh', inline=False)
       await message.channel.send(embed=embedVar)
 
