75829adbcf5f479a71f693e8802dfe9e96cb8493
Soul Sender <83854400+Soulsender@users.noreply.github.com>
2021-06-13 10:08:06 +0000

    Added Dice Rolling 1.0

--- main.py
+++ main.py
@@ -29,23 +29,19 @@ def get_serverplayers():
   say_serverplayers = json_data['players']['online']
   return say_serverplayers
 
-
-
-
 @client.event
 async def on_ready():
   print('{0.user} standing by'.format(client))
   await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='$help'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='In Maintenance'))
 
   @client.event
   async def on_message(message):
     if message.author == client.user:
       return
 
-
     if message.content.startswith('$insult'):
       await message.channel.send(get_insult())
-
 
     if message.content.startswith('$server'):
       players_online = get_serverplayers()
@@ -64,27 +60,70 @@ async def on_ready():
 
     # create basic replies
 
-    if message.content.startswith('$help'):
+    if message.content =='$help':
       embedVar = discord.Embed(title="__Command Menu__", color=0x2b2a2a)
-      embedVar.add_field(name="Useful", value="$help - send help menu \n $server - get infomation about our minecraft server \n $market - open the market (WIP)",inline=False)
+      embedVar.add_field(name="Useful", value="$help - send help menu \n $server - get infomation about our minecraft server \n $market - open the market (WIP) \n $roll - open dice roll menu",inline=False)
       embedVar.add_field(name="Miscellanous", value='$insult - send randomly generated insult \n $hello - say hello! \n $sheesh - sheesh \n $wap - no please dont', inline=False)
       await message.channel.send(embed=embedVar)
 
-    if message.content.startswith('$hello'):
+    if message.content == '$roll d20':
+      value = random.randint(1, 20)
+      await message.channel.send('**Rolled a D20!**')
+      if value == 20:
+        await message.channel.send('**NAT 20!**')
+      else:
+        await message.channel.send(value)
+
+    if message.content == '$roll d12':
+      value = random.randint(1, 12)
+      await message.channel.send('**Rolled a D12!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll d10':
+      value = random.randint(1, 10)
+      await message.channel.send('**Rolled a D10!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll d100':
+      value = random.randint(1, 100)
+      await message.channel.send('**Rolled a D100!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll d8':
+      value = random.randint(1, 8)
+      await message.channel.send('**Rolled a D8!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll d6':
+      value = random.randint(1, 6)
+      await message.channel.send('**Rolled a D6!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll d4':
+      value = random.randint(1, 4)
+      await message.channel.send('**Rolled a D4!**')
+      await message.channel.send(value)
+
+    if message.content == '$roll':
+      embedVar = discord.Embed(title="__Dice Roll Menu__", color=0x4287f5)
+      embedVar.add_field(name="Dice Types", value="D20 \n D12 \n D10 \n D100 \n D8 \n D6 \n D4 \n\n Use command with **$roll {dice}**",inline=False)
+      await message.channel.send(embed=embedVar)
+
+    if message.content == '$hello':
       await message.channel.send("Hello!")
 
-    if message.content.startswith('$wap'):
+    if message.content == '$wap':
       f = open("wop.txt", 'r')
       for word in f:
         await message.channel.send(word)
 
-    if message.content.startswith('cringe'):
+    if message.content == 'cringe':
       await message.channel.send("^ haha idiot")
 
-    if message.content.startswith('pog'):
+    if message.content == 'pog':
       await message.channel.send("https://tenor.com/view/lizard-dancing-poggers-lizard-dance-poggers-gif-18527737")
       
-    if message.content.startswith('$sheesh'):
+    if message.content == '$sheesh':
       await message.channel.send("sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesh")   
 
 keep_alive()
