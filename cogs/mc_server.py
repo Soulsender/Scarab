b14d69e27fcfeea7248f2fe3e4311319cdea390d
Soulsender <nexushopper3000@gmail.com>
2021-09-27 00:20:17 -0700

    re-organized functions into cogs

--- cogs/mc_server.py
+++ cogs/mc_server.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
 import requests
-import random
 import os
 
-class Useful(commands.Cog):
+class mc_server(commands.Cog):
   def __init__(self, client):
     self.client = client
 
@@ -44,19 +43,7 @@ class Useful(commands.Cog):
     embed.add_field(name="Players Online", value=str(players_online) + '/20', inline=False)
     await ctx.send(embed=embed)
 
-  @commands.command()
-  async def rollhelp(self, ctx):
-    embed = discord.Embed(title="__Dice Roll Menu__", color=0x1b006e)
-    embed.add_field(name="Dice Types", value="D20 \n D12 \n D10 \n D100 \n D8 \n D6 \n D4 \n\n Use command with **$roll {dice type}  {# of dice}** \n Ex. *$roll 20 3*",inline=False)
-    await ctx.send(embed=embed)
 
-  @commands.command()
-  async def roll(self, ctx, dicetype=20, dicenum=1):
-    if dicenum <= 20:
-      for _ in range(dicenum):
-        await ctx.send(random.randint(1, dicetype))
-    if dicenum > 20:
-      await ctx.send('Invalid number of rolls')
  
 def setup(client):
-  client.add_cog(Useful(client))
+  client.add_cog(mc_server(client))
